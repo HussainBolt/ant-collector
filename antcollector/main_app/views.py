0974462b28709bc3af370bb8e62e0b7bf62c71c6
HussainBolt <gotbasedhops@gmail.com>
2022-10-25 20:29:19 -0700

    2nd commit lab part 1 done

--- antcollector/main_app/views.py
+++ antcollector/main_app/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+class Ant:
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+ants = [
+  Ant('Lolo', 'tabby', 'foul little demon', 3),
+  Ant('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+  Ant('Raven', 'black tripod', '3 legged ant', 4)
+]
+
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
     return render(request, 'about.html')
+
+def ants_index(request):
+  return render(request, 'ants/index.html', { 'ants': ants })
